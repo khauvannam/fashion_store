23d769c812b3b48e39ac7daacda0990e4f263324
gN4HT <tranthang75142@gmail.com>
2024-12-06 18:32:40 +0700

    up

--- app/View/Pages/Collections.php
+++ app/View/Pages/Collections.php
@@ -7,23 +7,26 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Collection;
-use Livewire\Volt\Component;
+use Illuminate\View\Component;
 
 class Collections extends Component
 {
     public string $id;
     public Collection $products;
     public Collection $collection;
+    public array $category;
 
     public function mount(string $id, CategoryService $service): void
     {
         $this->id = $id;
-        $this->products = $service->show($id)->products()->get();
-        $this->collection = $service->show($id)->children()->get();
+        $this->category = $service->show($id);
+        dd($this->category);
     }
 
     public function render(): View|Factory|Application
     {
-        return view('pages.collections');
+        return view('pages.collections', [
+            'category' => $this->category
+        ]);
     }
 }
--- resources/views/pages/collections.blade.php
+++ resources/views/pages/collections.blade.php
@@ -1,3 +1,37 @@
 <x-app-layout>
-    <p>{{$id}}</p>
+    <div class="flex flex-col items-center justify-center py-10 bg-white">
+        <!-- Heading Section -->
+        <h1 class="text-3xl font-bold text-gray-800">{{ $collection['name'] }} CLOTHING COLLECTION</h1>
+        <p class="text-gray-500 text-center mt-2">
+          Find everything you need to look and feel your best, and shop the latest men's 
+          fashion and lifestyle products
+        </p>
+      
+        <!-- Button Group -->
+        <div class="flex space-x-4 mt-6">
+          <!-- Buttons -->
+          <button class="px-4 py-2 rounded-full bg-black text-white font-medium">
+            Tshirt
+          </button>
+          <button class="px-4 py-2 rounded-full border border-gray-400 text-gray-800 font-medium">
+            Jacket
+          </button>
+          <button class="px-4 py-2 rounded-full border border-gray-400 text-gray-800 font-medium">
+            Pants
+          </button>
+          <button class="px-4 py-2 rounded-full border border-gray-400 text-gray-800 font-medium">
+            Hoodie
+          </button>
+          <button class="px-4 py-2 rounded-full border border-gray-400 text-gray-800 font-medium">
+            Short
+          </button>
+          <button class="px-4 py-2 rounded-full border border-gray-400 text-gray-800 font-medium flex items-center space-x-2">
+            <span> 
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v18m9-9H3" />
+              </svg>
+            </span>
+          </button>
+        </div>
+      </div>      
 </x-app-layout>
