8d01657847ea8af89c42c1db4932085c94884293
khauvannam <nkhauvan@gmail.com>
2024-12-06 18:31:49 +0700

    product and categoryservice, footer, header blade

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-
 use App\View\Pages\Collections;
 use Illuminate\Support\Facades\Route;
 
 Route::view('', 'pages.home')->name('home');
+
 Route::get('collections/{id}', Collections::class)->name('collections');
 
 Route::view('dashboard', 'pages.dashboard')
