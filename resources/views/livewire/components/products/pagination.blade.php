e264edc00e63a0af0598f0a55e74b02e3bf5aff9
khauvannam <nkhauvan@gmail.com>
2024-12-07 20:00:17 +0700

    add pagination

--- resources/views/livewire/components/products/pagination.blade.php
+++ resources/views/livewire/components/products/pagination.blade.php
@@ -6,25 +6,81 @@ use Livewire\Volt\Component;
 
 new class extends Component {
     public int $totalPages;
-    public int $page = 0;
+    public int $currentPage = 1;
+    public array $pagination = [];
+
+
+    public function mount(): void
+    {
+        $this->getPagination();
+    }
 
     public function setPage(int $page): void
     {
-        $this->page = $page;
-        $this->dispatch('page-updated', currentPage: $this->page)->to('pages.products');
+        if ($this->currentPage == $page) return;
+        $this->currentPage = $page;
+        $this->getPagination();
+        $this->dispatch('page-updated', currentPage: $this->currentPage - 1)->to('pages.products');
+    }
+
+    public function getPagination(): void
+    {
+        $pagination = [];
+        // Add the first three pages or all if totalPages <= 3
+        for ($i = 1; $i <= min(3, $this->totalPages); $i++) {
+            $pagination[] = $i;
+        }
+
+        // Add "..." if the currentPage is beyond 4
+        if ($this->currentPage > 4) {
+            $pagination[] = -1; // Represent "..." placeholder
+        }
+
+        // Middle range around the currentPage
+        $start = max(4, $this->currentPage - 1);
+        $end = min($this->currentPage + 1, $this->totalPages - 3);
+
+        for ($i = $start; $i <= $end; $i++) {
+            $pagination[] = $i;
+        }
+
+        // Add "..." if there are pages beyond the visible range
+        if ($this->currentPage < $this->totalPages - 3) {
+            $pagination[] = -1; // Represent "..." placeholder
+        }
+
+        // Add the last three pages or more if the total pages are fewer
+        for ($i = max($this->totalPages - 2, 4); $i <= $this->totalPages; $i++) {
+            $pagination[] = $i;
+
+        }
+        $this->pagination = $pagination;
+
     }
 };
 
 ?>
 <div x-data="paginationScroll()" class="flex justify-center items-center mt-4 gap-5 my-5">
-    @for ($pageIndex = 0; $pageIndex < $totalPages; $pageIndex++)
+    <button
+        @click=" scrollToId('product-container')"
+        wire:click="setPage({{ 1 }})"
+        class="px-4 py-2 bg-gray-200 text-gray-800 rounded-md  ">
+        First
+    </button>
+    @foreach($pagination as $pag)
         <button
             @click=" scrollToId('product-container')"
-            wire:click="setPage({{ $pageIndex }})"
-            class="px-4 py-2 bg-gray-200 text-gray-800 rounded-md hover:bg-gray-300 {{ $pageIndex == $page ? 'bg-blue-500 text-white' : '' }}">
-            {{ $pageIndex + 1 }}
+            wire:click="setPage({{ $pag }})"
+            class="px-4 py-2 bg-gray-200 text-gray-800 rounded-md  {{ $pag == $currentPage ? 'bg-blue-500 text-white' : 'hover:bg-gray-300' }} ">
+            {{ $pag > 0 ? $pag : '...' }}
         </button>
-    @endfor
+    @endforeach
+    <button
+        @click=" scrollToId('product-container')"
+        wire:click="setPage({{ $totalPages }})"
+        class="px-4 py-2 bg-gray-200 text-gray-800 rounded-md  ">
+        Last
+    </button>
 </div>
 
 <script defer>
