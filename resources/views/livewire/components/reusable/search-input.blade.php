ea749ab7fefbd5aafe840565ab07f61fa3c2b820
gN4HT <tranthang75142@gmail.com>
2024-12-07 21:35:43 +0700

    search

--- resources/views/livewire/components/reusable/search-input.blade.php
+++ resources/views/livewire/components/reusable/search-input.blade.php
@@ -10,8 +10,8 @@ new class extends Component {
     }
 }; ?>
 
-<div class="absolute top-[80px] bg-white shadow-xl rounded-lg p-6 w-full z-50 flex flex-col gap-4">
-    <div class="flex items-center gap-4">
+<div class="absolute top-[80px] bg-white shadow-xl  rounded-lg p-6 w-full z-50 flex flex-col gap-4  border border-white">
+    <div class="flex items-center gap-4 ">
         <!-- Form -->
         <form action="{{ route('products') }}" method="GET" class="flex flex-1 items-center gap-4">
             <!-- Hidden Fields -->
@@ -29,23 +29,15 @@ new class extends Component {
                 name="search"
                 placeholder="Search for products..."
                 value="{{ request('search') }}"
-                class="w-full border border-gray-300 rounded-lg py-2 px-4 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                class="backdrop-blur-md w-full border-none placeholder-gray-500 text-gray-500 rounded-lg py-2 px-4 shadow-sm focus:outline-none focus:ring-0 focus:ring-transparent focus:border-transparent"
             />
-
-            <!-- Submit Button -->
-            <button
-                type="submit"
-                class="inline-flex items-center px-4 py-2 bg-indigo-600 text-white text-sm font-medium rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            >
-                Search
-            </button>
         </form>
 
         <!-- Close Button -->
         <button
             type="button"
             wire:click="toggleVisibility"
-            class="inline-flex items-center px-3 py-2 bg-gray-100 text-gray-500 rounded-lg hover:text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            class="inline-flex items-center px-3 py-2 text-gray-500 unded-lg hover:text-black focus:outline-none"
         >
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                 <path fill-rule="evenodd" d="M10 8.586L4.707 3.293a1 1 0 10-1.414 1.414L8.586 10l-5.293 5.293a1 1 0 001.414 1.414L10 11.414l5.293 5.293a1 1 0 001.414-1.414L11.414 10l5.293-5.293a1 1 0 00-1.414-1.414L10 8.586z" clip-rule="evenodd" />
