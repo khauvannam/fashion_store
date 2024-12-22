8c1bf5d3a637135be032d033c4dbfcb8b28111bb
khauvannam <nkhauvan@gmail.com>
2024-12-22 19:45:51 +0700

    Add file upload functionality to file-input component
    Implemented the ability to upload multiple images with validation for file type and size. Added UI feedback for uploaded image previews and error messages for validation failures. Files are stored in the "photos" directory on submission.

--- resources/views/livewire/components/reusable/inputs/file-input.blade.php
+++ resources/views/livewire/components/reusable/inputs/file-input.blade.php
@@ -1,11 +1,37 @@
 <?php
 
-use function Livewire\Volt\{state};
+use Livewire\Attributes\Validate;
+use Livewire\Volt\Component;
+use Livewire\WithFileUploads;
 
-state();
+new class extends component {
+    use WithFileUploads;
+
+    #[Validate(['photos.*' => 'image|max:5120'])]
+    public array $photos = [];
+
+    public function save(): void
+    {
+        foreach ($this->photos as $photo) {
+            $photo->store(path: 'photos');
+        }
+    }
+}
 
 ?>
 
 <div>
+    <form wire:submit="save">
+        <input type="file" wire:model="photos" multiple>
 
+        @error('photos.*') <span class="error">{{ $message }}</span> @enderror
+        @if ($photos)
+            @foreach($photos as $photo)
+                <div class="" wire:key="file-input_{{ $loop->index }}">
+                    <img src="{{ $photo->temporaryUrl() }}" alt="">
+                </div>
+            @endforeach
+        @endif
+        <button type="submit">Save photo</button>
+    </form>
 </div>
